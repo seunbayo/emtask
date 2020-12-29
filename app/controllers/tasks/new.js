304da5321ce3009eaccf8cfb26686ef0eae6b53b
seunbayo <seunbayo83@gmail.com>
2020-12-29 15:42:00 +0100

    added a model

--- app/controllers/tasks/new.js
+++ app/controllers/tasks/new.js
@@ -1,13 +1,6 @@
 import Controller from '@ember/controller';
 
 export default class TasksNewController extends Controller{
-    actions: {
-        addTask: function(){
-            var title = this.get('title');
-            var description = this.get('description');
-
-
+   
         }
 
-    }
-}
---
+++ app/models/task.js
@@ -0,0 +1,11 @@
+import Model, { attr } from '@ember-data/model';
+
+
+export default class TaskModel extends Model {
+    @attr('string') title;
+    @attr('date') date;
+    @attr('string') description;
+
+
+
+}
