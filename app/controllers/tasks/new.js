8b19ac9dad818fe59fbb1ff44b9e2f0dac7fa666
seunbayo <seunbayo83@gmail.com>
2020-12-30 10:11:51 +0100

    added ember fire

--- app/controllers/tasks/new.js
+++ app/controllers/tasks/new.js
@@ -1,6 +1,12 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
+import { action } from "@ember/object";
 
-export default class TasksNewController extends Controller{
-   
-        }
+export default class NewTaskController extends Controller {
+  @action
+  addTask() {
+    var title = this.get(`title`);
+    var description = this.get(`description`);
+    var date = this.get(`date`);
 
+  }
+}
--- app/models/task.js
+++ app/models/task.js
@@ -1,11 +1,7 @@
-import Model, { attr } from '@ember-data/model';
-
+import Model, { attr } from "@ember-data/model";
 
 export default class TaskModel extends Model {
-    @attr('string') title;
-    @attr('date') date;
-    @attr('string') description;
-
-
-
+  @attr("string") title;
+  @attr("date") date;
+    @attr("string") description;
 }
